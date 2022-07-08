9e221b0514541dbf895464ed0774d5c56bc0da5c
Saurabh Pund <80202824+saurabhpund@users.noreply.github.com>
2022-07-08 12:41:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const moreButton = document.getElementById('moreButton');
 const moreDiv = document.querySelector(".filterDiv .moreDiv")
+const filterDiv = document.querySelector(".filterDiv")
 moreButton.addEventListener("click", ()=>{
         if(moreButton.checked){
             moreDiv.style.display = "flex";
@@ -8,4 +9,17 @@ moreButton.addEventListener("click", ()=>{
             moreDiv.style.display = "none";
         }
     
+})
+
+
+const menu = document.querySelector("header .headerDiv .showIcon");
+const nav = document.querySelector("header nav")
+
+menu.addEventListener("click", ()=>{
+    if(nav.classList.contains("show")){
+        nav.classList.remove("show")
+    }
+    else{
+        nav.classList.add("show")
+    }
 })
